dcaa00d139cfc73efed4f59e7d6bd54b654fb153
tijan <tijanmdR@gmail.com>
2023-09-18 23:04:19 +1000

    create table using command done

--- foobar.php
+++ foobar.php
@@ -8,6 +8,28 @@ if(isset($options['help'])) {
     exit(0);
 }
 
+if (!isset($options['file'])) {
+    echo "Please specify a CSV file path with --file option";
+    exit(1);
+}
+
+if (isset($options['create_table'])) {
+    try {
+        $create_table = "create table if not exists users(
+            id int auto_increment primary key, 
+            name varchar(255) not null, 
+            surname varchar(255) not null, 
+            email varchar(255) not null unique, 
+        )";
+        $pdo->exec($create_table);
+        echo("Table 'users' created!");
+    } catch(PDOException $e) {
+        echo("Error: ".$e->getMessage());
+        exit(1);
+    } 
+    exit(0);
+}
+
 // Replace all the configuration with your local configuration
 $host = $options['h'] ?? 'localhost';
 $uname = $options['u'] ?? 'root';
