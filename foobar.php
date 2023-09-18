22b567e011e51e316af78e1120ff08e36c1af236
tijan <tijanmdR@gmail.com>
2023-09-18 21:55:43 +1000

    command line exectution for php file with host, uname and pwd done

--- foobar.php
+++ foobar.php
@@ -1,14 +1,16 @@
 <?php
 
+$options = getopt("h:u:p:");
+
 // Replace all the configuration with your local configuration
-$host = 'localhost';
-$uname = 'root';
-$pwd = '';
+$host = $options['h'] ?? 'localhost';
+$uname = $options['u'] ?? 'root';
+$pwd = $options['p'] ?? '';
 $db = 'test'; 
 
 // connect to the database
 try {
-    $conn = new PDO("mysql:host=$$host;dbname=$db", $uname, $pwd);
+    $conn = new PDO("mysql:host=$host;dbname=$db", $uname, $pwd);
     
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
