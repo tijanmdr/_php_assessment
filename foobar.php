f933e0ae46f79377f0332a415ecb9ea45c959b49
tijan <tijanmdR@gmail.com>
2023-09-19 22:12:44 +1000

    fixes

--- foobar.php
+++ foobar.php
@@ -8,32 +8,10 @@ if(isset($options['help'])) {
     exit(0);
 }
 
-if (!isset($options['u']) || !isset($options['p'])) {
-    echo "MySQL username and password is required. For more help: php foobar.php --help";
-    exit(1);
-}
-
-if (!isset($options['file'])) {
-    echo "Please specify a CSV file path with --file option. For more help: php foobar.php --help";
-    exit(1);
-}
-
-if (isset($options['create_table'])) {
-    try {
-        $create_table = "create table if not exists users(
-            id int auto_increment primary key, 
-            name varchar(255) not null, 
-            surname varchar(255) not null, 
-            email varchar(255) not null unique, 
-        )";
-        $pdo->exec($create_table);
-        echo("Table 'users' created!");
-    } catch(PDOException $e) {
-        echo("Error: ".$e->getMessage());
-        exit(1);
-    } 
-    exit(0);
-}
+// if (!isset($options['u']) || !isset($options['p'])) {
+//     echo "MySQL username and password is required. For more help: php foobar.php --help";
+//     exit(1);
+// }
 
 // Replace all the configuration with your local configuration
 $host = $options['h'] ?? 'localhost';
@@ -43,24 +21,49 @@ $db = 'test';
 
 // connect to the database
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$db", $uname, $pwd);
+    $pdo = new PDO("mysql:host=$host;dbname=$db", $uname, $pwd);
     
     // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connection successful";
 } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
     exit(1);
 }
 
+if (isset($options['create_table'])) {
+    try {
+        $create_table = "CREATE TABLE IF NOT EXISTS users(
+            id INT AUTO_INCREMENT PRIMARY KEY, 
+            name VARCHAR(255) NOT NULL, 
+            surname VARCHAR(255) NOT NULL, 
+            email VARCHAR(255) NOT NULL UNIQUE 
+        )";
+        $pdo->exec($create_table);
+        echo("Table 'users' created!");
+    } catch(PDOException $e) {
+        echo("Error: ".$e->getMessage());
+        exit(1);
+    } 
+    exit(0);
+} else if (!isset($options['file'])) {
+    echo "Please specify a CSV file path with --file option. For more help: php foobar.php --help";
+    exit(1);
+}
+
 if (($handle = fopen($options['file'], 'r')) !== false) {
     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-        $name = capitalizeFirstLetter($data[0]);
-        $surname = capitalizeFirstLetter($data[1]);
+        $name = ucfirst(strtolower(trim($data[0])));
+        $surname = ucfirst(strtolower(trim($data[1])));
         $email = strtolower(trim($data[2]));
 
         try {
-
+            $insert = "insert into users (name, surname, email) values (:n, :s, :e)";
+            $statement = $pdo->prepare($insert);
+            $statement->bindParam(':n', name);
+            $statement->bindParam(':s', surname);
+            $statement->bindParam(':e', email);
+            $statement->execute();
         } catch (PDOException $e) {
             echo "Error Message: ".$e->getMessage();
         }
