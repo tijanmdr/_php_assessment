3dfc8550549e78cd67502d667c8d356e3802379c
tijan <tijanmdR@gmail.com>
2023-09-19 22:46:27 +1000

    addded comments

--- foobar.php
+++ foobar.php
@@ -2,22 +2,23 @@
 
 $options = getopt("h:u:p:", ["file:", "create_table", "dry_run", "help"]);
 
+// trigger if there is --help arguement 
 if(isset($options['help'])) {
     echo "Commands: php foobar.php -u username -p password -h hostname --file csv_filepath [--create_table] [--dry_run]\n";
     echo "--file: Specify the CSV file name\n";
     exit(0);
 }
 
-if (!isset($options['u']) || !isset($options['p'])) {
-    echo "MySQL username and password is required. For more help: php foobar.php --help";
-    exit(1);
-}
+// if (!isset($options['u']) || !isset($options['p'])) {
+//     echo "MySQL username and password is required. For more help: php foobar.php --help";
+//     exit(1);
+// }
 
 // Replace all the configuration with your local configuration
 $host = $options['h'] ?? 'localhost';
 $uname = $options['u'] ?? 'root';
 $pwd = $options['p'] ?? '';
-$db = 'test'; 
+$db = 'test'; // replace your database name
 
 // connect to the database
 try {
@@ -31,6 +32,7 @@ try {
     exit(1);
 }
 
+// trigger if there is --create_table arguement 
 if (isset($options['create_table'])) {
     try {
         $create_table = "CREATE TABLE IF NOT EXISTS users(
@@ -46,18 +48,20 @@ if (isset($options['create_table'])) {
         exit(1);
     } 
     exit(0);
-} else if (!isset($options['file'])) {
+} else if (!isset($options['file'])) { // Don't need to trigger unless there is no --create_table arguement
     echo "Please specify a CSV file path with --file option. For more help: php foobar.php --help\n";
     exit(1);
 }
 
+// open and read the csv file
 if (($handle = fopen($options['file'], 'r')) !== false) {
-    $errors = 0;
+    $errors = 0; // to count the sql errors 
     while (($data = fgetcsv($handle, 1000, ",")) !== false) {
         $name = ucfirst(strtolower(trim($data[0])));
         $surname = ucfirst(strtolower(trim($data[1])));
         $email = strtolower(trim($data[2]));
 
+        // to validate the email address
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             if ($email === 'email') {continue;}
             echo "Invalid email found: $email\n";
@@ -65,6 +69,7 @@ if (($handle = fopen($options['file'], 'r')) !== false) {
             continue;
         }
         try {
+            // insert data to the table
             $insert = "INSERT INTO users 
                 (name, surname, email) VALUES (:n, :s, :e)
             ";
