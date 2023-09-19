f4debe7087adb54e926a7f0fc62eb62533b0faa1
tijan <tijanmdR@gmail.com>
2023-09-19 22:57:29 +1000

    fix

--- foobar.php
+++ foobar.php
@@ -2,16 +2,25 @@
 
 for ($i=1; $i<=100; $i++) {
     $out = $i;
+
+    // divisible by 3
     if ($i % 3 ===0) {
         $out = "foo";
     }
-    if ($i% 5 === 0) 
-        $out = "bar";
 
+    // divisible by 5
+    if ($i% 5 === 0) {
+        $out = "bar";
+    }
+
+    // divisible by 3 and 5
     if ($i% 5 === 0 && $i%3 === 0) {
         $out = 'foobar';
     } 
+
     echo $out;
+
+    // print comma unless the number is 100
     if ($i !== 100) {
         echo ", ";
     }
