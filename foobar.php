c4ebe5a7d6f1d60443095b1815acae73a3af3b93
tijan <tijanmdR@gmail.com>
2023-09-19 22:56:30 +1000

    sequence done

--- foobar.php
+++ foobar.php
@@ -1,7 +1,17 @@
 <?php
 
 for ($i=1; $i<=100; $i++) {
-    echo $i;
+    $out = $i;
+    if ($i % 3 ===0) {
+        $out = "foo";
+    }
+    if ($i% 5 === 0) 
+        $out = "bar";
+
+    if ($i% 5 === 0 && $i%3 === 0) {
+        $out = 'foobar';
+    } 
+    echo $out;
     if ($i !== 100) {
         echo ", ";
     }
